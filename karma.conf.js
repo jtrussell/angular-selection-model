5cafd7dc89f88434cf5fd77814425ae096a333bb
Justin Russell <jus.russell@gmail.com>
2014-02-25 22:39:36 -0500

    chore: Add jQuery (dev) for easier testing

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
     singleRun: false,
     frameworks: ['jasmine'],
     files: [
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'src/scripts/*.js',
