cedbbacf9347fe5d16b81458b8ead33c9c9ea032
Justin Russell <jus.russell@gmail.com>
2014-06-29 21:43:37 -0400

    fix: Remove from selected items list on $destroy
    Make sure we don't leave things in the selected items list when they are removed
    from the repeated over collection of the cleanup strategy is set to "deselect".
    
    See #17

--- examples/vendor/angular-selection-model/selection-model.js
+++ examples/vendor/angular-selection-model/selection-model.js
@@ -263,6 +263,26 @@ angular.module('selectionModel').directive('selectionModel', [
           scope.$apply();
         };
 
+        /**
+         * Routine to keep the list of selected items up to date
+         *
+         * Adds/removes this item from `selectionModelSelectedItems`.
+         */
+        var updateSelectedItemsList = function() {
+          if(angular.isArray(selectedItemsList)) {
+            var ixSmItem = selectedItemsList.indexOf(smItem);
+            if(smItem[selectedAttribute]) {
+              if(-1 === ixSmItem) {
+                selectedItemsList.push(smItem);
+              }
+            } else {
+              if(-1 < ixSmItem) {
+                selectedItemsList.splice(ixSmItem, 1);
+              }
+            }
+          }
+        };
+
         element.on('click', handleClick);
         if('checkbox' === smType) {
           var elCb = element.find('input');
@@ -279,6 +299,7 @@ angular.module('selectionModel').directive('selectionModel', [
         if('deselect' === cleanupStrategy) {
           scope.$on('$destroy', function() {
             smItem[selectedAttribute] = false;
+            updateSelectedItemsList();
           });
         }
 
@@ -288,20 +309,7 @@ angular.module('selectionModel').directive('selectionModel', [
             deselectAllItems();
             smItem[selectedAttribute] = true;
           }
-
-          if(angular.isArray(selectedItemsList)) {
-            var ixSmItem = selectedItemsList.indexOf(smItem);
-            if(smItem[selectedAttribute]) {
-              if(-1 === ixSmItem) {
-                selectedItemsList.push(smItem);
-              }
-            } else {
-              if(-1 < ixSmItem) {
-                selectedItemsList.splice(ixSmItem, 1);
-              }
-            }
-          }
-
+          updateSelectedItemsList();
           updateDom();
         });
       }
